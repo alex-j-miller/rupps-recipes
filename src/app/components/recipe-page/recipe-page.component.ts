e2282165fc5ee800465e9f4164b42224da607612
Jaden Brookens <jlbrookens@gmail.com>
2024-12-11 22:26:59 -0500

    initial things

--- src/app/components/recipe-page/recipe-page.component.ts
+++ src/app/components/recipe-page/recipe-page.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-
+import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
 @Component({
   selector: 'app-recipe-page',
   standalone: true,
-  imports: [],
+  imports: [MatDividerModule, MatListModule],
   templateUrl: './recipe-page.component.html',
-  styleUrl: './recipe-page.component.css'
+  styleUrl: './recipe-page.component.css',
 })
-export class RecipePageComponent {
-
-}
+export class RecipePageComponent {}
